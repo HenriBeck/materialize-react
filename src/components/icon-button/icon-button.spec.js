94bfa6b413cc33beb97d7565118da97eb9083823
Henri Beck <henribeck.dev@gmail.com>
2017-02-23 13:36:50 +0100

    Added full coverage of IconButton

--- src/components/icon-button/icon-button.spec.js
+++ src/components/icon-button/icon-button.spec.js
@@ -34,6 +34,42 @@ test('should have aria-disabled and tabIndex of -1 when disabled', (t) => {
   t.deepEqual(button.prop('tabIndex'), -1);
 });
 
+test('should call the event handlers when the mouse or a touch event happens', (t) => {
+  const onMouseDown = sinon.spy();
+  const onTouchStart = sinon.spy();
+  const wrapper = shallow(
+    <IconButton
+      icon="github"
+      onMouseDown={onMouseDown}
+      onTouchStart={onTouchStart}
+    />,
+  );
+
+  wrapper.simulate('mouseDown');
+  wrapper.simulate('touchStart');
+
+  t.deepEqual(onMouseDown.callCount, 1);
+  t.deepEqual(onTouchStart.callCount, 1);
+});
+
+test('should call the onPress handler when a mouse or touch event happens', (t) => {
+  const onPress = sinon.spy();
+  const wrapper = shallow(
+    <IconButton
+      icon="github"
+      onPress={onPress}
+    />,
+  );
+
+  wrapper.simulate('mouseDown');
+
+  t.deepEqual(onPress.callCount, 1);
+
+  wrapper.simulate('touchStart');
+
+  t.deepEqual(onPress.callCount, 2);
+});
+
 test('should add and remove the focus from the ripple', (t) => {
   const onFocus = sinon.spy();
   const onBlur = sinon.spy();
@@ -52,4 +88,73 @@ test('should add and remove the focus from the ripple', (t) => {
   wrapper.simulate('blur');
 
   t.deepEqual(onBlur.callCount, 1);
+});
+
+test('should add focus to the ripple when the component get\'s focused', (t) => {
+  const wrapper = mount(<IconButton icon="github" />);
+  const instance = wrapper.instance();
+
+  instance.ripple.addFocus = sinon.spy();
+  instance.ripple.removeFocus = sinon.spy();
+
+  wrapper.simulate('focus');
+
+  t.deepEqual(instance.ripple.addFocus.callCount, 1);
+
+  wrapper.simulate('blur');
+
+  t.deepEqual(instance.ripple.removeFocus.callCount, 1);
+});
+
+test('should not call the onPress handler when the keyCode isn\'t specified', (t) => {
+  const onPress = sinon.spy();
+  const wrapper = mount(
+    <IconButton
+      icon="github"
+      onPress={onPress}
+    />,
+  );
+
+  wrapper.simulate('keyDown');
+
+  t.deepEqual(onPress.callCount, 0);
+});
+
+test('should only call the onPress handler when the keyCode is specified and valid', (t) => {
+  const onPress = sinon.spy();
+  const wrapper = mount(
+    <IconButton
+      icon="github"
+      onPress={onPress}
+    />,
+  );
+
+  t.plan(IconButton.keyCodes.length);
+
+  IconButton.keyCodes.forEach((keyCode, index) => {
+    wrapper.simulate('keyDown', { keyCode });
+
+    t.deepEqual(onPress.callCount, index + 1);
+
+    wrapper.simulate('keyUp');
+  });
+});
+
+test('should not call the onPress handler again if the keyUp handler hasn\'t been called', (t) => {
+  const onPress = sinon.spy();
+  const wrapper = mount(
+    <IconButton
+      icon="github"
+      onPress={onPress}
+    />,
+  );
+  const keyCode = IconButton.keyCodes[0];
+
+  wrapper.simulate('keyDown', { keyCode });
+
+  t.deepEqual(onPress.callCount, 1);
+
+  wrapper.simulate('keyDown', { keyCode });
+
+  t.deepEqual(onPress.callCount, 1);
 });
--- src/components/tabs/tabs.spec.js
+++ src/components/tabs/tabs.spec.js
@@ -4,13 +4,17 @@ import test from 'ava';
 import Tabs from './tabs.jsx';
 import { shallow } from 'tests/enzyme';
 
-test('should throw an error if no children are passed', (t) => {
-  t.throws(() => shallow(<Tabs />));
+test('should throw an error if only one child is passed', (t) => {
+  t.throws(() => shallow(
+    <Tabs initialTabId="tab1">
+      <div />
+    </Tabs>,
+  ));
 });
 
 test('should throw an error if a children is not a tab', (t) => {
   t.throws(() => shallow(
-    <Tabs>
+    <Tabs initialTabId="tab1">
       <div />
       <div />
     </Tabs>,
