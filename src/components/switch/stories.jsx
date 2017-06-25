7799ddcec0256fc8127d6150703d0e00bb321e0f
Henri Beck <henribeck.dev@gmail.com>
2017-06-25 16:13:24 +0200

    Added stories to switch component

--- src/components/switch/stories.jsx
+++ src/components/switch/stories.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import {
+  text,
+  boolean,
+} from '@storybook/addon-knobs';
 
 import Switch from './switch-container';
 
 storiesOf('Switch', module)
   .add('Default Styles', () => (
     <Switch name="test" />
+  ))
+  .add('Default Toggled', () => (
+    <Switch
+      defaultToggled
+      name="test"
+    />
+  ))
+  .add('Disabled', () => (
+    <Switch
+      disabled
+      name="test"
+    />
+  ))
+  .add('Label right', () => (
+    <Switch name="test">Label</Switch>
+  ))
+  .add('Label left', () => (
+    <Switch
+      labelPosition="left"
+      name="test"
+    >
+      Label
+    </Switch>
+  ))
+  .add('Interactive', () => (
+    <Switch
+      disabled={boolean('Disabled', false)}
+      name="test"
+    >
+      {text('Label', 'Label')}
+    </Switch>
   ));
