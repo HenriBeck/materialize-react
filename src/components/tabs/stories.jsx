08430188e98386121260e312c229ae28339c38cb
Henri Beck <henribeck.dev@gmail.com>
2017-07-13 12:42:47 +0200

    Fixed the prop name for the initial tab in the stories

--- src/components/tabs/stories.jsx
+++ src/components/tabs/stories.jsx
@@ -6,7 +6,7 @@ import Tab from '../tab';
 
 storiesOf('Tabs', module)
   .add('Default styles', () => (
-    <Tabs defaultSelected="test2">
+    <Tabs initialTab="test2">
       <Tab name="test1">Test 1</Tab>
       <Tab name="test2">Test 2</Tab>
       <Tab name="test3">Test 3</Tab>
