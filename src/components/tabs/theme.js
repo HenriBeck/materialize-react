9e020e9acec40b9919ffad4d23b91f9e3b5dde2e
Henri Beck <henribeck.dev@gmail.com>
2017-07-13 13:42:18 +0200

    Updated the transition of the bar

--- src/components/tabs/theme.js
+++ src/components/tabs/theme.js
@@ -1,6 +1,9 @@
 import PropTypes from 'prop-types';
 
-export const schema = PropTypes.shape({ barColor: PropTypes.string.isRequired });
+export const schema = PropTypes.shape({
+  barColor: PropTypes.string.isRequired,
+  transitionDuration: PropTypes.number.isRequired,
+});
 
 /**
  * Default theme for the tabs.
@@ -9,5 +12,8 @@ export const schema = PropTypes.shape({ barColor: PropTypes.string.isRequired })
  * @returns {Object} - Returns the theme.
  */
 export function defaultTheme(vars) {
-  return { barColor: vars.primaryBase };
+  return {
+    barColor: vars.primaryBase,
+    transitionDuration: vars.transitionTime * 2,
+  };
 }
