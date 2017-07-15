917e4856171656bdc51369e3f103f7f87c7424a0
Henri Beck <henribeck.dev@gmail.com>
2017-07-16 01:20:05 +0200

    Added tests for the tabs container

---
+++ src/components/tabs/tabs-container.spec.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import test from 'ava';
+import sinon from 'sinon';
+
+import TabsContainerWrapper, { TabsContainer } from './tabs-container';
+import { shallow } from '../../../tests/helpers/enzyme';
+
+test('should render a Jss hoc', (t) => {
+  const props = {
+    className: '',
+    noBar: false,
+    createRef: () => {},
+    onFocus: () => {},
+    onBlur: () => {},
+    onKeyPress: () => {},
+  };
+  const wrapper = shallow(<TabsContainerWrapper {...props}>Children</TabsContainerWrapper>);
+  const jssContainer = wrapper.find('Jss(TabsContainer)');
+
+  t.deepEqual(jssContainer.length, 1);
+  t.deepEqual(jssContainer.dive().find('TabsContainer').length, 1);
+});
+
+test('should render an EventHandler as a root', (t) => {
+  const props = {
+    classes: {},
+    className: '',
+    noBar: false,
+    createRef: sinon.spy(),
+    onFocus: () => {},
+    onBlur: () => {},
+    onKeyPress: () => {},
+  };
+  const wrapper = shallow(<TabsContainer {...props}>Children</TabsContainer>);
+
+  t.deepEqual(wrapper.find('EventHandler').length, 1);
+  t.deepEqual(props.createRef.callCount, 1);
+});
+
+test('should set the root prop of the instance', (t) => {
+  const props = {
+    classes: {},
+    className: '',
+    noBar: false,
+    createRef: () => {},
+    onFocus: () => {},
+    onBlur: () => {},
+    onKeyPress: () => {},
+  };
+  const wrapper = shallow(<TabsContainer {...props}>Children</TabsContainer>);
+  const instance = wrapper.instance();
+
+  instance.createRootRef('root');
+
+  t.deepEqual(instance.root, 'root');
+});
+
+test('should set the bar prop of the instance', (t) => {
+  const props = {
+    classes: {},
+    className: '',
+    noBar: false,
+    createRef: () => {},
+    onFocus: () => {},
+    onBlur: () => {},
+    onKeyPress: () => {},
+  };
+  const wrapper = shallow(<TabsContainer {...props}>Children</TabsContainer>);
+  const instance = wrapper.instance();
+  const eventHandler = wrapper.find('EventHandler').dive();
+
+  eventHandler.find('span').node.ref('bar');
+
+  t.deepEqual(instance.bar, 'bar');
+});
