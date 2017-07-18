ae9e52b90c3e9bf0e87ed57d211cfeba9e1daf03
Henri Beck <henribeck.dev@gmail.com>
2017-07-18 05:52:24 +0200

    Removed unnecessary jss plugins

--- src/styles/jss.js
+++ src/styles/jss.js
@@ -4,21 +4,17 @@ import cache from 'jss-cache';
 import camelCase from 'jss-camel-case';
 import compose from 'jss-compose';
 import defaultUnit from 'jss-default-unit';
-import expand from 'jss-expand';
 import nested from 'jss-nested';
 import propsSort from 'jss-props-sort';
 import vendorPrefixer from 'jss-vendor-prefixer';
-import global from 'jss-global';
 
 const jss = createJss();
 
-jss.use(global());
 jss.use(cache());
 jss.use(nested());
 jss.use(compose());
 jss.use(camelCase());
 jss.use(defaultUnit({ 'line-height': 'px' }));
-jss.use(expand());
 jss.use(vendorPrefixer());
 jss.use(propsSort());
 
