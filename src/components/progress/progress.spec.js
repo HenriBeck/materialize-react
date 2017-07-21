26e614f42212c7472ea02dd59992da10eb0c2b81
Henri Beck <henribeck.dev@gmail.com>
2017-07-21 22:00:18 +0200

    Fixed progress tests

--- src/components/progress/progress.spec.js
+++ src/components/progress/progress.spec.js
@@ -1,50 +1,29 @@
 import test from 'ava';
 import React from 'react';
-import { shallow } from 'enzyme';
 
-import ProgressWrapper, { Progress } from './progress';
+import ProgressWrapper from './progress';
 import { mount } from '../../../tests/helpers/enzyme';
-
-const defaultProps = {
-  classes: { progress: 'progress' },
-  theme: { progress: {} },
-};
 
 test('should have a root node with the role of progressbar', (t) => {
   const wrapper = mount(<ProgressWrapper />);
 
   // Check if we render a JSS HoC
   t.deepEqual(wrapper.find('Jss(Progress)').length, 1);
+  t.deepEqual(wrapper.find('.progress').length, 1);
 });
 
-test('should have aria-valuemin and aria-valuemax on the root node', (t) => {
-  const wrapper = shallow(<Progress {...defaultProps} />);
+test('should have aria-valuemin and aria-valuemax and aria-valuenow on the root node', (t) => {
+  const wrapper = mount(<ProgressWrapper progress={40} />);
   const root = wrapper.find('.progress');
 
   // Check if the valuemin and valuemax props are set
   t.deepEqual(root.prop('aria-valuemin'), 0);
+  t.deepEqual(root.prop('aria-valuenow'), 40);
   t.deepEqual(root.prop('aria-valuemax'), 100);
 });
 
-test('should set the aria-valuenow to the value of the progress prop', (t) => {
-  const wrapper = shallow(
-    <Progress
-      progress={40}
-      {...defaultProps}
-    />,
-  );
-  const root = wrapper.find('.progress');
-
-  t.deepEqual(root.prop('aria-valuenow'), 40);
-});
-
 test('should change the aria-valuenow when the progress prop changes', (t) => {
-  const wrapper = shallow(
-    <Progress
-      progress={40}
-      {...defaultProps}
-    />,
-  );
+  const wrapper = mount(<ProgressWrapper progress={40} />);
 
   wrapper.setProps({ progress: 100 });
 
@@ -55,43 +34,32 @@ test('should change the aria-valuenow when the progress prop changes', (t) => {
 });
 
 test('should animate the secondary progress bar', (t) => {
-  const wrapper = shallow(
-    <Progress
-      secondaryProgress={40}
-      {...defaultProps}
-    />,
-  );
+  const wrapper = mount(<ProgressWrapper secondaryProgress={40} />);
 
   wrapper.setProps({ secondaryProgress: 80 });
 
-  // There is no way of exactly checking if the secondary progress has been animated
-  t.pass();
+  const style = wrapper.find('.progress--secondary-bar').node.style;
+
+  t.deepEqual(style.transform, 'matrix(0.8, 0, 0, 1, 0, 0)');
 });
 
 test('should add the indeterminate class', (t) => {
-  const wrapper = shallow(
-    <Progress
-      indeterminate
-      active
-      {...defaultProps}
-    />,
-  );
+  const wrapper = mount(<ProgressWrapper indeterminate />);
   const elem = wrapper.find('.progress');
 
   // Check if the correct class has been applied
   t.true(elem.prop('className').includes('progress--indeterminate'));
+
+  t.deepEqual(elem.prop('data-active'), false);
+
+  wrapper.setProps({ active: true });
 
   // Check if the element got updated
   t.true(elem.prop('data-active'));
 });
 
 test('should have aria-disabled set to true', (t) => {
-  const wrapper = shallow(
-    <Progress
-      disabled
-      {...defaultProps}
-    />,
-  );
+  const wrapper = mount(<ProgressWrapper disabled />);
   const root = wrapper.find('.progress');
 
   // Check if the aria-disabled props is set
