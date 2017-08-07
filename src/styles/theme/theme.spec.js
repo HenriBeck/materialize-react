669d5232570bb0a1a11c6d953c609948a4a72393
Henri Beck <henribeck.dev@gmail.com>
2017-08-07 17:25:56 +0200

    Fixed tests for the Theme component

--- src/styles/theme/theme.spec.js
+++ src/styles/theme/theme.spec.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import test from 'ava';
 import is from 'is_js';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 import Theme, { compileTheme } from './theme';
 
-test('should just render a Theme Provider', (t) => {
-  const wrapper = shallow(
+test('should render a Jss Hoc and a Theme Provider', (t) => {
+  const wrapper = mount(
     <Theme>
       <div>
         Children
@@ -14,6 +14,7 @@ test('should just render a Theme Provider', (t) => {
     </Theme>,
   );
 
+  t.deepEqual(wrapper.find('Jss(Theme)').length, 1);
   t.deepEqual(wrapper.find('ThemeProvider').length, 1);
 });
 
