30bd6043d895e71474ad64754765f775a4808f2f
Henri Beck <henribeck.dev@gmail.com>
2017-08-09 18:25:54 +0200

    Created components to add some styling for different Modal content
    Added components:
    - ModalButtons (for a button row at the bottom of the modal)
    - ModalContent (for the main content for the modal)
    - ModalHeader (the header for the modal)

---
+++ src/components/modal/modal-content.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import injectSheet from 'react-jss';
+import PropTypes from 'prop-types';
+
+/**
+ * A component which adds the appropriate styles for the content inside a modal.
+ *
+ * @param {Object} props - The props for the component.
+ * @param {Object} props.classes - Classes provided by Jss.
+ * @param {String} props.className - Additional className.
+ * @param {JSX} props.children - The actual content for the modal.
+ * @returns {JSX} - Returns the jsx.
+ */
+export function ModalContent({
+  classes,
+  className,
+  children,
+  ...props
+}) {
+  return (
+    <main
+      className={`${classes.content} ${className}`}
+      {...props}
+    >
+      {children}
+    </main>
+  );
+}
+
+ModalContent.propTypes = {
+  classes: PropTypes.shape({ content: PropTypes.string.isRequired }).isRequired,
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+};
+
+ModalContent.defaultProps = { className: '' };
+
+ModalContent.styles = ({ modal: theme }) => {
+  return {
+    content: {
+      composes: 'modal--content',
+      padding: theme.padding,
+    },
+  };
+};
+
+export default injectSheet(ModalContent.styles)(ModalContent);
---
+++ src/components/modal/modal-header.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import injectSheet from 'react-jss';
+import PropTypes from 'prop-types';
+
+/**
+ * A component which adds the appropriate styles for the header of a modal.
+ *
+ * @param {Object} props - The props for the component.
+ * @param {Object} props.classes - Classes provided by Jss.
+ * @param {String} props.className - Additional className.
+ * @param {JSX} props.children - The content for the header.
+ * @returns {JSX} - Returns the jsx.
+ */
+export function ModalHeader({
+  classes,
+  className,
+  children,
+  ...props
+}) {
+  return (
+    <header
+      className={`${classes.header} ${className}`}
+      {...props}
+    >
+      {children}
+    </header>
+  );
+}
+
+ModalHeader.propTypes = {
+  classes: PropTypes.shape({ content: PropTypes.string.isRequired }).isRequired,
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+};
+
+ModalHeader.defaultProps = { className: '' };
+
+ModalHeader.styles = {
+  header: {
+    composes: 'modal--header',
+    marginBottom: 20,
+  },
+};
+
+export default injectSheet(ModalHeader.styles)(ModalHeader);
