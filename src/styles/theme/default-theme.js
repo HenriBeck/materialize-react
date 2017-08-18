471bef81bd5aa66059b9ad7fbd6193ab3d46f10e
Henri Beck <henribeck.dev@gmail.com>
2017-08-18 15:55:33 +0200

    Added Typography component

--- src/styles/theme/default-theme.js
+++ src/styles/theme/default-theme.js
@@ -33,6 +33,7 @@ import { defaultTheme as tab } from '../../components/tab/theme';
 import { defaultTheme as drawer } from '../../components/drawer/theme';
 import { defaultTheme as toolbar } from '../../components/toolbar/theme';
 import { defaultTheme as snackbar } from '../../components/snackbar/theme';
+import { defaultTheme as typography } from '../../components/typography/theme';
 
 export const defaultVars = {
   primaryBase: blue500,
@@ -78,4 +79,5 @@ export const defaultTheme = {
   drawer,
   toolbar,
   snackbar,
+  typography,
 };
