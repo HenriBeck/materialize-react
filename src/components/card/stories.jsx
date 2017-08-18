6b27b6cbe4d515d1779ac8d211d8cd88ca90496a
Henri Beck <henribeck.dev@gmail.com>
2017-08-18 12:18:59 +0200

    Added Stories

--- src/components/card/stories.jsx
+++ src/components/card/stories.jsx
@@ -3,26 +3,138 @@ import { storiesOf } from '@storybook/react';
 
 import Card from './card';
 import Button from '../button';
+import { blue500 } from '../../styles/colors';
 
 const avatar = (
-  <span style={{ backgroundColor: 'red' }}>
+  <span style={{ backgroundColor: blue500 }}>
     HB
   </span>
 );
 
 storiesOf('Card', module)
-  .add('Default', () => (
+  .add('Title with content', () => (
+    <Card>
+      <Card.Header>Title</Card.Header>
+
+      <Card.Content>
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+      </Card.Content>
+    </Card>
+  ))
+  .add('Title with subtitle and content', () => (
+    <Card>
+      <Card.Header subtitle="Subtitle">Title</Card.Header>
+
+      <Card.Content>
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+      </Card.Content>
+    </Card>
+  ))
+  .add('Title with avatar and content', () => (
+    <Card>
+      <Card.Header avatar={avatar}>Title</Card.Header>
+
+      <Card.Content>
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+      </Card.Content>
+    </Card>
+  ))
+  .add('Title with avatar and subtitle and content', () => (
     <Card>
       <Card.Header
         avatar={avatar}
-        subtitle="Subtitle"
+        subtitle="subtitle"
       >
         Title
       </Card.Header>
 
-      <Card.Actions stacked>
-        <Button>Test</Button>
-        <Button>Test</Button>
+      <Card.Content>
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+      </Card.Content>
+    </Card>
+  ))
+  .add('Title with content and actions', () => (
+    <Card>
+      <Card.Header>Title</Card.Header>
+
+      <Card.Content>
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+      </Card.Content>
+
+      <Card.Actions>
+        <Button>Action 1</Button>
+
+        <Button>Action 2</Button>
       </Card.Actions>
+    </Card>
+  ))
+  .add('Title with content and actions stacked', () => (
+    <Card>
+      <Card.Header>Title</Card.Header>
+
+      <Card.Content>
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+      </Card.Content>
+
+      <Card.Actions stacked>
+        <Button>Action 1</Button>
+
+        <Button>Action 2</Button>
+      </Card.Actions>
+    </Card>
+  ))
+  .add('Image with title and content', () => (
+    <Card>
+      <Card.Media url="http://placehold.it/400x250" />
+
+      <Card.Header subtitle="subtitle">Title</Card.Header>
+
+      <Card.Content>
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+      </Card.Content>
+    </Card>
+  ))
+  .add('Title with image and action', () => (
+    <Card>
+      <Card.Header>Title</Card.Header>
+
+      <Card.Media url="http://placehold.it/400x250" />
+
+      <Card.Content>
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+        Lorem ipsum dolor, Lorem ipsum dolor
+      </Card.Content>
     </Card>
   ));
