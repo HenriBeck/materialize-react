b89b010e3049beb8dd7db79c1af52d0eb3ecc496
Henri Beck <henribeck.dev@gmail.com>
2017-08-21 12:23:16 +0200

    Updated createClassesFromStyles to support passing a function

--- tests/helpers/create-classes-from-styles.js
+++ tests/helpers/create-classes-from-styles.js
@@ -1,12 +1,23 @@
+import { defaultTheme } from '../../src/styles/theme/default-theme';
+
+const themeArgument = Object
+  .keys(defaultTheme)
+  .reduce((theme, component) => Object.assign({}, theme, { [component]: {} }));
+
 /**
  * Create a classes object from the styles for testing purposes.
  *
- * @param {Object} styles - The styles from which the classes will be computed.
+ * @param {(Object|Function)} styles - The styles from which the classes will be computed.
  * @returns {Object} - Returns an object with the classes.
  */
 export default function createClassesFromStyles(styles) {
+  if (typeof styles === 'function') {
+    return createClassesFromStyles(styles(themeArgument));
+  }
+
   return Object
     .keys(styles)
+    .filter(className => styles[className].composes)
     .map(className => Object.assign({ [className]: styles[className].composes }))
     .reduce((classes, classObj) => Object.assign({}, classes, classObj), {});
 }
