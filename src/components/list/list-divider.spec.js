8f733db81e8855127a248c9488cc36e3ecad3957
Henri Beck <henribeck.dev@gmail.com>
2017-08-29 19:02:04 +0200

    Added tests for List Divider

---
+++ src/components/list/list-divider.spec.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import test from 'ava';
+
+import ListDivider from './list-divider';
+import { mount } from '../../../tests/helpers/enzyme';
+
+test('should render a li and a Divider inside', (t) => {
+  const wrapper = mount(<ListDivider />);
+  const li = wrapper.find('li');
+
+  t.deepEqual(li.length, 1);
+  t.deepEqual(li.find('Divider').length, 1);
+});
+
+test('should add a class of list--divider-inset when the inset prop is passed', (t) => {
+  const wrapper = mount(<ListDivider inset />);
+  const li = wrapper.find('li');
+
+  t.deepEqual(li.prop('className').includes('list--divider-inset'), true);
+});
