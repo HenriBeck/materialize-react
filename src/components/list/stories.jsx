d81f1f768d87542acb9687442e484cfc8942e453
Henri <henribeck.dev@gmail.com>
2017-08-30 05:24:30 +0200

    Update stories.jsx

--- src/components/list/stories.jsx
+++ src/components/list/stories.jsx
@@ -105,13 +105,13 @@ storiesOf('List', module)
         leftItem={<List.Item.Avatar><Icon icon="account-circle" /></List.Item.Avatar>}
         rightItem={<Icon icon="information" />}
       >
-        List item 1
+        List item 2
       </List.Item>
       <List.Item
         leftItem={<List.Item.Avatar><Icon icon="account-circle" /></List.Item.Avatar>}
         rightItem={<Icon icon="information" />}
       >
-        List item 1
+        List item 3
       </List.Item>
     </List>
   ))
