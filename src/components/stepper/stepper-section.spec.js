31415a14a777fe819fa0d770a8a8cf943978a90a
Henri Beck <henribeck.dev@gmail.com>
2017-09-02 16:47:44 +0200

    Fixed stepper section tests

--- src/components/stepper/stepper-section.spec.js
+++ src/components/stepper/stepper-section.spec.js
@@ -6,9 +6,7 @@ import { mount } from '../../../tests/helpers/enzyme';
 import StepperSection from './stepper-section';
 
 test('should render a div with the class `stepper--section`', (t) => {
-  const wrapper = mount(<StepperSection>Test</StepperSection>);
-  const div = wrapper.find('div');
+  const wrapper = mount(<StepperSection name="test">Test</StepperSection>);
 
-  t.deepEqual(div.length, 1);
-  t.deepEqual(div.prop('className').includes('stepper--section'), true);
+  t.deepEqual(wrapper.find('div.stepper--section').length, 1);
 });
