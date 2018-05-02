68c86fd0d8bb06c2a6b52bfaef81583be372cb94
Henri Beck <henribeck.dev@gmail.com>
2018-05-02 19:47:25 +0200

    Updated eslint config to set the node env for test files
    Signed-off-by: Henri Beck <henribeck.dev@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,4 +8,10 @@ module.exports = {
 
   // Because we use flow, react/require-default-props doesn't play well with it
   rules: { 'react/require-default-props': 'off' },
+
+  overrides: [{
+    files: [ "*.spec.js"],
+
+    env: { node: true },
+  }]
 };
