c8f821ec2e9b7903e66644810238387990c9bcdb
Henri Beck <henribeck.dev@gmail.com>
2018-05-03 18:39:11 +0200

    Updated the storybook config
    Signed-off-by: Henri Beck <henribeck.dev@gmail.com>

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,16 +1,25 @@
 // @flow strict
 
-module.exports = { // eslint-disable-line import/no-commonjs, import/unambiguous
-  module: {
-    rules: [{
-      test: /\.(ttf|eot|svg|woff(2)?)(\?.+)?$/, // eslint-disable-line unicorn/no-unsafe-regex
-      loader: 'file-loader',
-    }, {
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-      ],
-    }],
-  },
+const path = require("path");
+
+module.exports = (storybookBaseConfig) => {
+  return Object.assign({}, storybookBaseConfig, {
+    module: {
+      rules: [{
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        include: [ path.resolve(__dirname, '..') ],
+        exclude: [ path.resolve(__dirname, '../node_modules') ],
+      }, {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?.+)?$/, // eslint-disable-line unicorn/no-unsafe-regex
+        loader: 'file-loader',
+      }, {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      }],
+    }
+  });
 };
