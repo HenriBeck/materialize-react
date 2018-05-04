781030c06a10110b8dc98bb7ef88804ac47d37d5
Henri Beck <henribeck.dev@gmail.com>
2018-05-04 18:47:56 +0200

    Finished tests for the getCoords function
    Signed-off-by: Henri Beck <henribeck.dev@gmail.com>

--- src/utils/get-coords.spec.js
+++ src/utils/get-coords.spec.js
@@ -6,20 +6,32 @@ import getCoords from './get-coords';
 
 test('should return null when no coordinates were found ', (t) => {
   t.deepEqual(
-    getCoords({}),
+    getCoords(new FocusEvent('focus')),
     null,
   );
 });
 
-test('should return the coords when the are', (t) => {
-  t.deepEqual(
-    getCoords({
+test('should return the coords when it\'s a mouse event', (t) => {
+  // Simulated mouse event
+  const ev = new MouseEvent('mousedown');
+
+  t.deepEqual(getCoords(ev), {
+    x: ev.clientX,
+    y: ev.clientY,
+  });
+});
+
+test('should return the coords when it\'s a touch event', (t) => {
+  // Simulated touch event
+  const ev = new TouchEvent('touchstart', {
+    touches: [{
       clientX: 5,
       clientY: 5,
-    }),
-    {
-      x: 5,
-      y: 5,
-    },
-  );
+    }],
+  });
+
+  t.deepEqual(getCoords(ev), {
+    x: ev.touches[0].clientX,
+    y: ev.touches[0].clientY,
+  });
 });
