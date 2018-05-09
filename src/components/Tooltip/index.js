735deb2c532a8f5eee9ebcaccf879b455bcd2738
Henri Beck <henribeck.dev@gmail.com>
2018-05-09 19:11:54 +0200

    Started working on the implementation Started on the styles
    Signed-off-by: Henri Beck <henribeck.dev@gmail.com>

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -2,6 +2,8 @@
 
 import React, { type Node } from 'react';
 
+import Sheet, { type Data } from './Sheet';
+
 type Position = 'top-start'
   | 'top'
   | 'top-end'
@@ -17,11 +19,45 @@ type Position = 'top-start'
 type Props = {
   children: Node,
   content: Node,
-  location: Position,
+  position: Position,
 };
+type State = { show: boolean };
 
-export default class Tooltip extends React.PureComponent<Props> {
+export type { Position };
+
+export default class Tooltip extends React.PureComponent<Props, State> {
+  state = { show: false };
+
+  handleMouseEnter = () => {
+    this.setState({ show: true });
+  };
+
+  handleMouseLeave = () => {
+    this.setState({ show: false });
+  };
+
   render() {
+    const data: Data = {
+      position: this.props.position,
+      show: this.state.show,
+    };
 
+    return (
+      <Sheet data={data}>
+        {({ classes }) => (
+          <div
+            className={classes.container}
+            onMouseEnter={this.handleMouseEnter}
+            onMouseLeave={this.handleMouseLeave}
+          >
+            {this.props.children}
+
+            <div className={classes.tooltip}>
+              {this.props.content}
+            </div>
+          </div>
+        )}
+      </Sheet>
+    );
   }
 }
