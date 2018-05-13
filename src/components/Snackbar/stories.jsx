d0abb77443e0e1b7d6b145093d27c5106e460f04
Henri Beck <henribeck.dev@gmail.com>
2018-05-13 14:36:03 +0200

    Updated Snackbar component to be aligned with the new Material Design Spec
    Signed-off-by: Henri Beck <henribeck.dev@gmail.com>

--- src/components/Snackbar/stories.jsx
+++ src/components/Snackbar/stories.jsx
@@ -1,52 +1,52 @@
 // @flow strict
 
-import React, {
-  type Element,
-  type ElementType,
-} from 'react';
+import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import Button from '../Button';
+import Layout from '../Layout';
+import Checkbox from '../Checkbox';
+import Label from '../Label';
+import TextArea from '../TextArea';
 
 import Snackbar from '.';
 import Container from './Container';
 
 type Props = {};
-type State = { snackbars: $ReadOnlyArray<Element<ElementType>> };
+type State = {
+  snackbars: $ReadOnlyArray<{
+    id: number,
+    withAction: boolean,
+    message: string,
+    isAnimatingOut: boolean,
+  }>,
+  withAction: boolean,
+  message: string,
+};
 
 class Story extends React.PureComponent<Props, State> {
-  state = { snackbars: [] };
+  state = {
+    snackbars: [],
+    withAction: false,
+    message: 'Single-line message',
+  };
 
   id = 0;
 
-  handlePress = name => () => {
+  handleAddPress = () => {
     this.id += 1;
 
-    if (name === 'basic') {
-      this.setState((state) => {
-        return {
-          snackbars: [...state.snackbars, (
-            <Snackbar key={this.id}>
-              A basic snackbar
-            </Snackbar>
-          )],
-        };
-      });
-    } else if (name === 'with-action') {
-      this.setState((state) => {
-        return {
-          snackbars: [...state.snackbars, (
-            <Snackbar
-              key={this.id}
-              action={(<Button onPress={action('Snackbar action')}>Action</Button>)}
-            >
-              A Snackbar with an action
-            </Snackbar>
-          )],
-        };
-      });
-    }
+    this.setState((state) => {
+      return {
+        snackbars: [...state.snackbars, {
+          id: this.id,
+          withAction: state.withAction,
+          message: state.message,
+          isAnimatingOut: false,
+        }],
+      };
+    });
   };
 
   /**
@@ -54,9 +54,56 @@ class Story extends React.PureComponent<Props, State> {
    */
   handleRemoveSnackbar = () => {
     this.setState((state) => {
-      return { snackbars: state.snackbars.slice(1) };
+      const [, ...snackbars] = state.snackbars;
+
+      return { snackbars };
     });
   };
+
+  handleWithActionChange = () => {
+    this.setState((state) => {
+      return { withAction: !state.withAction };
+    });
+  };
+
+  handleMessageChange = (ev) => {
+    this.setState({ message: ev.target.value });
+  };
+
+  handleClosePress = () => {
+    this.handleCloseRequest();
+  };
+
+  handleCloseRequest = (...args) => {
+    action('Close Request')(...args);
+
+    this.setState((state) => {
+      return {
+        snackbars: state.snackbars.map((snackbar, index) => {
+          return {
+            ...snackbar,
+            isAnimatingOut: index === 0,
+          };
+        }),
+      };
+    });
+  };
+
+  renderSnackbars() {
+    return this.state.snackbars.map(snackbar => (
+      <Snackbar
+        key={snackbar.id}
+        isAnimatingOut={snackbar.isAnimatingOut}
+        action={snackbar.withAction ? (
+          <Button onPress={action('Action Press')}>Action</Button>
+        ) : null}
+        onCloseRequest={this.handleCloseRequest}
+        onRemoveRequest={this.handleRemoveSnackbar}
+      >
+        {snackbar.message}
+      </Snackbar>
+    ));
+  }
 
   render() {
     return (
@@ -67,14 +114,34 @@ class Story extends React.PureComponent<Props, State> {
         }}
       >
         <Container>
-          {this.state.snackbars}
+          {this.renderSnackbars()}
         </Container>
 
-        <div>
-          <Button onPress={this.handlePress('basic')}>Add snackbar</Button>
+        <Layout
+          direction="column"
+          crossAlign="flex-start"
+        >
+          <Label
+            control={(
+              <Checkbox
+                checked={this.state.withAction}
+                onChange={this.handleWithActionChange}
+              />
+            )}
+          >
+            With Action
+          </Label>
 
-          <Button onPress={this.handlePress('with-action')}>Add snackbar with action</Button>
-        </div>
+          <TextArea
+            label="Snackbar message"
+            value={this.state.message}
+            onChange={this.handleMessageChange}
+          />
+
+          <Button onPress={this.handleAddPress}>Add snackbar</Button>
+
+          <Button onPress={this.handleClosePress}>Close snackbar</Button>
+        </Layout>
       </div>
     );
   }
